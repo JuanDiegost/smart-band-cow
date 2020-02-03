d0a78e5d33f392492c2a257949460d1008405fcc
JuanDiegost <molinajuan149@gmail.com>
2020-02-02 22:15:46 -0500

    rasberry complete and app base

--- codigo-rasberry-pi/index.js
+++ codigo-rasberry-pi/index.js
@@ -1,24 +1,37 @@
-
 const admin = require('firebase-admin');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(bodyParser.json());
+
+app.listen(3000, () => {
+  console.log('Started on PORT 3000');
+});
 
 const serviceAccount = require('./smart-cow.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://smart-cow-d6c6f.firebaseio.com',
+  databaseURL: 'https://smart-band-fdf82.firebaseio.com',
 });
 
-const db = admin.firestore();
-
-function getValue() {
-  return Math.random();
-}
-
-setInterval(() => {
-  console.log('nuevo dato');
-  const docRef = db.collection('datos').doc(Date.now().toString());
-  docRef.set({
-    date: Date.now(),
-    value: getValue(),
-  });
-}, 5000);
+app.post('/add', function (req, res) {
+  console.log(req.body);
+  const id = req.body.id;
+  const value = req.body.value;
+  const ref = admin.database().ref('/datos/id');
+  console.log("id = " + id + ", value is " + value);
+  var obj = {
+    value: value,
+    date: Date.now()
+  };
+  ref.push(obj);
+  res.end("yes");
+});
