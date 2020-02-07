333e6a85f8d4a0deb05cf05fe53582dea949e128
JuanDiegost <molinajuan149@gmail.com>
2020-02-07 12:56:07 -0500

    version final

--- codigo-rasberry-pi/index.js
+++ codigo-rasberry-pi/index.js
@@ -27,7 +27,7 @@ admin.initializeApp({
 
 app.post("/add", function(req, res) {
   const id = req.body.id;
-  const value = (req.body.value - Math.random() * (50 - 20) + 30).toFixed(0);
+  const value = req.body.value - Math.random() * (50 - 20) + 30;
   const ref = admin.database().ref("/datos/id");
   if (isT) console.log("id = " + id + ", value is " + value);
   else console.log("id = " + id + ", value is " + 0);
